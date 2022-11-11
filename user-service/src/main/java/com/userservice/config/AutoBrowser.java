4a4abda5e0c30a684ad8b5d88fd9ed87b9767b8a
brother-long-Lin <1269967980@qq.com>
2022-11-11 17:34:06 +0800

    11月11日 前后端分离

---
+++ user-service/src/main/java/com/userservice/config/AutoBrowser.java
@@ -0,0 +1,24 @@
+package com.userservice.config;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+
+import java.io.IOException;
+
+@Configuration
+public class AutoBrowser {
+    @EventListener({ApplicationReadyEvent.class})
+    void applicationReadyEvent() {
+        //System.out.println("应用已经准备就绪 ... 启动浏览器");
+        // 这里需要注url:端口号+测试类方法名
+        String url = "http://localhost:8081/static/User/Login.html";
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
